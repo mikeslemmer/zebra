4614b6909fb5d6a2a2d8cea43421b51bb71a3167
Mike Slemmer <mslemmer@yahoo-inc.com>
2014-04-03 22:15:57 -0700

    more work

--- test.rb
+++ test.rb
@@ -14,11 +14,19 @@ end
 
 sock = TCPSocket.open(hostname, port)
 
+# Start out by removing the test key.
+sock.puts "del test"
+sock.gets
+
 sock.puts "blah"
 check_result(sock, "(error) ERR unknown command 'blah'")
 
 sock.puts "zadd test 1 first 2 second 3 third"
 check_result(sock, "(integer) 3")
+
+sock.puts "zrange 0 3"
+check_result(sock, "1) \"first\"")
+
 
 sock.close
 
